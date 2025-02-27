302bca22a5c943f80e8ccf1b4b446952ccd10de6
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-02-26 20:11:34 -0500

    Create FixDebugThree3.java

---
+++ FixDebugThree3.java
@@ -0,0 +1,8 @@
+public class FixDebugThree3 {
+    public static void main(String[] args) {
+        int num1 = 125, num2 = 50, num3 = 200, average;
+        average = (num1 + num2 + num3) / 3;
+
+        System.out.println("The average is " + average);
+    }
+}
