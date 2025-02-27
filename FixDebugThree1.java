a4dc5f39fe1ee59091da6cfda581d6c44099e724
Chowdhury85 <145401540+Chowdhury85@users.noreply.github.com>
2025-02-26 20:10:17 -0500

    Create FixDebugThree1.java

---
+++ FixDebugThree1.java
@@ -0,0 +1,8 @@
+public class FixDebugThree1 {
+    public static void main(String[] args) {
+        System.out.println("Over the river and through the woods");
+        System.out.println("To Grandmother's house we go");
+        System.out.println("The horse knows the way to carry the sleigh");
+        System.out.println("Through the white and drifted snow.");
+    }
+}
